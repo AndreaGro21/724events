99635a2118e3a293b31b1918f6a3c89b5bbbf458
andrea gronchi <andreagronchi21@gmail.com>
2023-09-22 15:47:34 +0200

    "fixed 10k commits error"

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("./events.json");
+    console.log(json, "cristo")
+    return json.json();
+  },
+};
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const getData = useCallback(async () => {
+    try {
+      setData(await api.loadData());
+    } catch (err) {
+      setError(err);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);//added ^ for disable empty array from effect
+  useEffect(() => {
+    if (data) return;
+    getData();
+  });
+  console.log(data, "diocane")
+  console.log(getData, "cacca")
+  console.log({ children })
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+      }}
+    >
+
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const useData = () => {
+  const {data, error} = useContext(DataContext)
+
+  const last = data?.focus?.[data.focus.length -1] // récupération derniere élément de focus
+
+  return {data, error, last}
+}
+
+export default DataContext;
